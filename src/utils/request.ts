547fc13d9f928cdcb6662c715a5e47e8700623dc
Trung Nguyen <chitrung.nguyen@vietlinkads.com>
2022-06-01 10:57:00 +0700

    Update axios request function

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,19 +1,75 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import axios, { AxiosRequestConfig } from 'axios'
+import axios, { AxiosRequestConfig, Method, AxiosRequestHeaders } from 'axios'
 
-export const request = async (url = '', method = '', data = {}, params = {}) => {
-  const headers = {
-    'Content-Type': 'application/json;charset=UTF-8',
-    'Access-Control-Allow-Origin': '*'
+enum StatusCode {
+  Unauthorized = 401,
+  Forbidden = 403,
+  BadRequest = 404,
+  TooManyRequests = 429,
+  InternalServerError = 500
+}
+
+const handleError = error => {
+  const { response: { status = null } = {} } = error
+
+  switch (status) {
+    case StatusCode.InternalServerError: {
+      // Handle InternalServerError
+      break
+    }
+    case StatusCode.Forbidden: {
+      // Handle Forbidden
+      break
+    }
+    case StatusCode.BadRequest: {
+      // Handle BadRequest
+      alert('Bad request. Please try again!')
+      break
+    }
+    case StatusCode.Unauthorized: {
+      // Handle Unauthorized
+      break
+    }
+    case StatusCode.TooManyRequests: {
+      // Handle TooManyRequests
+      break
+    }
   }
 
+  return error
+}
+
+const headers: AxiosRequestHeaders = {
+  'Content-Type': 'application/json;charset=UTF-8',
+  'Access-Control-Allow-Origin': '*'
+}
+
+export const request = async (url: string, method: Method, data?: any, params?: any): Promise<any> => {
   const config: AxiosRequestConfig = {
     baseURL: process.env.REACT_APP_URL,
     headers,
     params
   }
 
+  const injectToken = (config: AxiosRequestConfig): AxiosRequestConfig => {
+    try {
+      const token = localStorage.getItem('accessToken')
+
+      if (token != null) {
+        config.headers = {
+          Authorization: `Bearer ${token}`
+        }
+      }
+
+      return config
+    } catch (error: any) {
+      throw new Error(error)
+    }
+  }
+
   const instance: any = axios.create(config)
+
+  instance.interceptors.request.use(injectToken, error => Promise.reject(error))
 
   instance.interceptors.response.use(
     config => config,
@@ -24,9 +80,9 @@ export const request = async (url = '', method = '', data = {}, params = {}) => 
   try {
     const getMethod: string = method.toLowerCase()
     const res = await instance[getMethod](url, data)
-    // const res = await instance.get(url, data)
+
     return res.data
-  } catch (e) {
-    return e
+  } catch (error) {
+    return handleError(error)
   }
 }
