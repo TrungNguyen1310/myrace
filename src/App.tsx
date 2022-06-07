d5e17451abdcdab1876d688178f5b15c78bfea5f
Trung Nguyen <chitrung.nguyen@vietlinkads.com>
2022-06-07 09:59:30 +0700

    Add suspense and lazy

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import { ProtectedRoutes } from 'routes/ProtectedRoutes'
 
 import './App.scss'
 import { PrivateListRoutes } from 'routes/ListRoutes/protected'
 import { PublicListRoutes } from 'routes/ListRoutes/public'
+import LoadingSpinner from 'components/LoadingSpinner'
 
-const isAuth = 's'
+const isAuth = ''
 
 function App(): JSX.Element {
   return (
-    <Routes>
-      {/* _________________ PROTECTED ROUTES _________________ */}
-      <Route element={<ProtectedRoutes isAuth={isAuth} />}>{PrivateListRoutes()}</Route>
+    <Suspense fallback={<LoadingSpinner />}>
+      <Routes>
+        {/* _________________ PROTECTED ROUTES _________________ */}
+        <Route element={<ProtectedRoutes isAuth={isAuth} />}>{PrivateListRoutes()}</Route>
 
-      {/* _________________ PUBLIC ROUTES _________________ */}
-      <Route>{PublicListRoutes(isAuth)}</Route>
-    </Routes>
+        {/* _________________ PUBLIC ROUTES _________________ */}
+        <Route>{PublicListRoutes(isAuth)}</Route>
+      </Routes>
+    </Suspense>
   )
 }
 
