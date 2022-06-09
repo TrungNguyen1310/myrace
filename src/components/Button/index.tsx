96ae6b3ca2c72eb3a8adb237dc94e215c1d427c8
Trung Nguyen <chitrung.nguyen@vietlinkads.com>
2022-06-09 13:53:26 +0700

    Custom button style

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { ReactNode } from 'react'
 import { Button as MuiButton } from '@mui/material'
+import { styled } from '@mui/material/styles'
 
 interface BtnProps {
   children: ReactNode
@@ -10,15 +11,23 @@ interface BtnProps {
   primary?: boolean
 }
 
+const ButtonStyled = styled(MuiButton)(() => ({
+  '&:disabled': {
+    backgroundColor: '#b7b7b74f',
+    cursor: 'not-allowed',
+    pointerEvents: 'all'
+  }
+}))
+
 const VlButton: React.FC<BtnProps | any> = ({ children = null, className = '', primary = false, border = false, borderColor = 'border-vl_grey-400', ...props }) => {
-  const baseStyle = 'rounded-[23px] h-[40px] p-[12px] normal-case text-vl_black font-bold text-xs xl:text-sm'
+  const baseStyle = 'rounded-[23px] h-[40px] p-[12px] normal-case text-vl_black dark:text-white font-bold text-xs xl:text-sm'
   const borderStyle = border ? `border-solid border ${borderColor}` : ''
   const primaryStyle = primary ? 'border-solid border border-vl_grey-500 dark:text-white' : ''
 
   return (
-    <MuiButton disableRipple {...props} className={[baseStyle, primaryStyle, borderStyle, className].join(' ')}>
+    <ButtonStyled {...props} className={[baseStyle, primaryStyle, borderStyle, className].join(' ')}>
       {children}
-    </MuiButton>
+    </ButtonStyled>
   )
 }
 
