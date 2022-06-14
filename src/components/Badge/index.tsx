67adabed313c281d38ef981fa25f4bb20c2cc0f7
Trung Nguyen <chitrung.nguyen@vietlinkads.com>
2022-06-14 16:46:55 +0700

    Update Badge props and logic

--- src/components/Badge/index.tsx
+++ src/components/Badge/index.tsx
@@ -13,25 +13,17 @@ interface IBadge {
   showZero?: boolean
 }
 
-const StyledBadge = styled(Badge)<IBadge>(({ theme, bgcolor, primary }) => ({
-  '& .MuiBadge-badge': {
-    backgroundColor: bgcolor || (primary ? theme.palette.color.primary.primary_blue : theme.palette.color.vl_white),
-    color: primary ? theme.palette.color.vl_white.DEFAULT : theme.palette.color.vl_black.DEFAULT,
-    fontSize: theme.palette.fontSize.xs,
-    fontWeight: 700,
-    lineHeight: theme.palette.lineHeight.xs,
-    border: primary ? 'none' : `1px solid ${theme.palette.color.vl_grey[500]}`,
-    minWidth: '26px',
-    minHeight: '18px',
-    height: 'auto',
-    padding: '0 9px'
-  }
-}))
+const VlBadge: React.FC<IBadge> = ({ children, className, primary, bgcolor, ...props }) => {
+  const baseStyle = `${primary ? 'vl-badge-primary' : 'vl-badge-base'}`
 
-const VlBadge: React.FC<IBadge> = ({ children, className, ...props }) => {
-  const baseStyle = `${props.primary ? 'vl-badge-primary' : 'vl-badge'}`
+  const StyledBadge = styled(Badge)<IBadge>(({ theme }) => ({
+    '& .MuiBadge-badge': {
+      backgroundColor: bgcolor || (primary ? theme.palette.color.primary.primary_blue : theme.palette.color.vl_white.DEFAULT)
+    }
+  }))
+
   return (
-    <StyledBadge {...props} className={[baseStyle, className].join(' ')}>
+    <StyledBadge {...props} className={['vl-badge', baseStyle, className].join(' ')}>
       {children}
     </StyledBadge>
   )
