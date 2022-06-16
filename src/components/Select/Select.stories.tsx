a658ddc426e078e7cb74974c95954adf8d1e25ef
Trung Nguyen <nguyenchitrung1310@gmail.com>
2022-06-16 22:25:50 +0700

    Create select story

---
+++ src/components/Select/Select.stories.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+import Select from '../Select'
+
+export default {
+  title: 'Components/Select',
+  component: Select,
+  args: {
+    disabled: false,
+    options: [
+      {
+        value: 'apple',
+        label: 'Apple'
+      },
+      {
+        value: 'lemon',
+        label: 'Lemon'
+      },
+      {
+        value: 'melon',
+        label: 'melon'
+      }
+    ]
+  }
+} as ComponentMeta<typeof Select>
+
+const Template: ComponentStory<typeof Select> = args => <Select {...args} />
+
+export const Outlined = Template.bind({})
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Outlined.args = {
+  label: 'Juice',
+  placeholder: 'Select juice...'
+}
+
+export const Standard = Template.bind({})
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Standard.args = {
+  label: 'Juice',
+  placeholder: 'Select juice...',
+  variant: 'standard'
+}
