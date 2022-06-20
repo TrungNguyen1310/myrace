129d6578bb52f05588de13242ad1d870902e9caa
Trung Nguyen <nguyenchitrung1310@gmail.com>
2022-06-20 23:31:40 +0700

    Update footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -9,11 +9,16 @@ import Button from 'components/Button'
 
 const Footer: React.FC = () => {
   const { i18n, t } = useTranslation()
-  const [currency, setCurrency] = useState<string>('1')
+  const [currency, setCurrency] = useState<string>('vnd')
   const [language, setLanguage] = useState<string>('en')
   const options = [
     { label: t('components.footer.language.english'), value: 'en' },
     { label: t('components.footer.language.german'), value: 'de' }
+  ]
+
+  const options1 = [
+    { label: t('components.footer.currency.usd'), value: 'usd' },
+    { label: t('components.footer.currency.vnd'), value: 'vnd' }
   ]
   const [theme, setTheme] = useContext(ThemeContext)
 
@@ -50,13 +55,9 @@ const Footer: React.FC = () => {
         <p className='text-right-footer'>{t('components.footer.copywrite')}</p>
       </div>
       {/* ======= RIGHT ======= */}
-      {/* <div className='flex items-center justify-between relative bg-pink-400 w-full' /> */}
       <div className='flex items-center justify-between relative'>
-        {/* <Select variant='standard' disableUnderline value={currency} onChange={e => onChangeSelect(e, 'currency')}>
-          <MenuItem value='1'>{t('components.footer.currency.usd')}</MenuItem>
-          <MenuItem value='2'>{t('components.footer.currency.vnd')}</MenuItem>
-        </Select> */}
-        <Select placement='top' defaultValue='en' options={options} onChange={opt => onChangeSelect(opt, 'language')} className='px-[41px]' classNameSelect='lang-select' />
+        <Select defaultValue={currency} options={options1} onChange={opt => onChangeSelect(opt, 'currency')} className='select-footer' classNameSelect='currency-select' />
+        <Select defaultValue={language} options={options} onChange={opt => onChangeSelect(opt, 'language')} className='px-[41px] select-footer' classNameSelect='lang-select' />
         <Button shape='circle' onClick={toggleDarkMode} className={`${theme === 'dark' && 'darkmode'} border-solid border border-vl_grey-500 w-[46px] h-[46px]`}>
           <MoonIcon />
         </Button>
