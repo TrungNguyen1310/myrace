2e7794bf8c37e3d63985a2b74313e599ca044d3d
Trung Nguyen <chitrung.nguyen@vietlinkads.com>
2022-06-21 11:50:17 +0700

    Create badge component

---
+++ src/components/Badge/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './style.scss'
+
+interface IBadge {
+  badgeContent: string | number
+  className?: string
+  classNameBadge?: string
+  bgcolor?: string
+  primary?: boolean
+  max?: number
+  showZero?: boolean
+}
+
+const Badge: React.FC<IBadge> = ({ badgeContent, className, classNameBadge, showZero, max = 99, primary, ...props }) => {
+  const baseStyle = `${primary ? 'vl-badge-primary' : 'vl-badge-base'}`
+
+  if ((badgeContent === 0 || badgeContent === '0') && !showZero) return null
+  return (
+    <div className={className}>
+      <div {...props} className={['vl-badge', baseStyle, classNameBadge].join(' ')}>
+        {+badgeContent < max ? badgeContent : `${max}+`}
+      </div>
+    </div>
+  )
+}
+
+export default Badge
--- src/features/Home/index.tsx
+++ src/features/Home/index.tsx
@@ -3,6 +3,7 @@ import Select from 'components/Select'
 import Input from 'components/Input'
 import { ReactComponent as SocialIcon } from 'assets/icons/social.svg'
 import Button from 'components/Button'
+import Badge from 'components/Badge'
 
 const Home = () => {
   const [value, setvalue] = useState<string>('')
@@ -29,6 +30,15 @@ const Home = () => {
 
   return (
     <div>
+      <h3>Badge</h3>
+      <div className='flex items-center'>
+        <Badge className='px-2' classNameBadge='dark:bg-secondary_pink dark:text-vl_white' badgeContent={4} />
+        <Badge className='px-2' badgeContent={4} primary />
+        <Badge className='px-2' badgeContent={110} showZero />
+        <Badge className='px-2' badgeContent={200} showZero max={199} />
+      </div>
+      <br />
+      <h3>Button</h3>
       <div className='flex items-end mb-4'>
         <Button>Submit</Button>
         <Button disabled>Disabled</Button>
@@ -46,11 +56,13 @@ const Home = () => {
           Search
         </Button>
       </div>
+      <h3>Select</h3>
       <Select labelStyle='text-red-400' className='mb-10' variant='outlined' options={options} onChange={onChangeSelect} label='Juice' />
       <Select labelStyle='text-primary_purple' variant='standard' options={options1} onChange={onChangeSelect} label='ABC' />
       <Select defaultValue='vn' variant='outlined' options={options2} onChange={onChangeSelect} label='Country' />
       <br />
       <br />
+      <h3>Input</h3>
       <Input label='Email' value={value} onChange={e => setvalue(e.target.value)} placeholder='Type...' icon={<SocialIcon />} />
       <Input classNameInput='bg-red-300' className='ml-20 text-sky-500' labelStyle='text-red-300' label='Text' value={value} onChange={e => setvalue(e.target.value)} placeholder='Type...' />
       <Input disabled label='Text' value={value} onChange={e => setvalue(e.target.value)} placeholder='Type...' />
